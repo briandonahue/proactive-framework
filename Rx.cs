d8de28d78af6c1c23137fe3bd2af1c6c7ca17aae
frank.alva.krueger <frank.alva.krueger@5328fce2-1293-11df-902e-ef6c94960d2c>
2010-02-05 20:40:30 +0000

    Work on AbstractObservable. Now only Run() is virtual. Made Observables property more efficient by caching the result. Added protection to make sure Run() isn't called multiple times.

--- Rx.cs
+++ Rx.cs
@@ -38,9 +38,17 @@ namespace Rx
 			});
 		}
 
-		//public static IObservable<TResult> SelectMany<TSource, TResult> (this IObservable<TSource> source, Func<TSource, IObservable<TResult>> selector)
-		//{
-		//}
+		public static IObservable<TResult> SelectMany<TSource, TResult> (this IObservable<TSource> source, Func<TSource, IObservable<TResult>> selector)
+		{
+			return new DelegateObservable<TResult>(onNext => {
+				source.Subscribe(value => {
+					var o = selector(value);
+					o.Subscribe(value2 => {
+						onNext(value2);
+					});
+				});
+			});
+		}
 
 		public static IObservable<TResult> SelectMany<TSource, TCollection, TResult> (this IObservable<TSource> source, Func<TSource, IObservable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
 		{
@@ -147,56 +155,76 @@ namespace Rx
 	
 	public abstract class AbstractObservable<T> : IObservable<T> {
 		
-		List<IObserver<T>> _observers = new List<IObserver<T>>();		
+		List<IObserver<T>> _observers = new List<IObserver<T>>();
+		IObserver<T>[] _observersCache = null;
 		object _observersLock = new object();
+		
+		bool _started = false;
+		object _startedLock = new object();
 		
 		public AbstractObservable() {
 		}
 			
-		public virtual void Subscribe (IObserver<T> observer)
+		public void Subscribe (IObserver<T> observer)
 		{
 			lock (_observersLock) {
 				_observers.Add(observer);
+				_observersCache = null;
 			}
 			Start();
 		}
-		
-		void Start() {
-			System.Threading.ThreadPool.QueueUserWorkItem(delegate {
-				Run();
-			});
-		}
-		
+				
 		protected abstract void Run();
 		
-		protected IObserver<T>[] GetObservers() {
-			lock (_observersLock) {
-				return _observers.ToArray();
-			}
-		}
-		
-		protected virtual void OnNext(T value) {
+		protected void OnNext(T value) {
 			Observable.Synchronize(delegate {
-				foreach (var o in GetObservers()) {
+				foreach (var o in Observers) {
 					o.OnNext(value);
 				}
 			});
 		}
 		
-		protected virtual void OnCompleted() {
+		protected void OnCompleted() {
 			Observable.Synchronize(delegate {
-				foreach (var o in GetObservers()) {
+				foreach (var o in Observers) {
 					o.OnCompleted();
 				}
 			});
 		}
 		
-		protected virtual void OnError(Exception error) {
+		protected void OnError(Exception error) {
 			Observable.Synchronize(delegate {
-				foreach (var o in GetObservers()) {
+				foreach (var o in Observers) {
 					o.OnError(error);
 				}
 			});
+		}
+		
+		IObserver<T>[] Observers {
+			get {
+				var cache = _observersCache;
+				if (cache == null) {
+					lock (_observersLock) {
+						cache = _observersCache;
+						if (cache == null) {
+							cache = _observers.ToArray();
+							_observersCache = cache;
+						}
+					}
+				}
+				return cache;				
+			}
+		}
+		
+		void Start() {
+			lock (_startedLock) {
+				if (!_started) {
+					System.Threading.ThreadPool.QueueUserWorkItem(delegate {
+						Run();
+					});
+					_started = true;
+				}		
+		    }
 		}
 	}
 }
